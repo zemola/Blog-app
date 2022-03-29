82cfb571568a86fc2233d83c1ce5b03b025f0de0
unknown <azeemolatunji@gmail.com>
2022-03-29 13:31:57 +0100

    Made changes as requested by the reviwer

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  after_save :update_post_counter
+
+  private
+
   def update_posts_counter
-    user.increment!(:posts_counter)
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
