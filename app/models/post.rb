34df5e158507f00443e6d50ef68da77a47834239
unknown <azeemolatunji@gmail.com>
2022-04-01 11:00:12 +0100

    setup validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :comment_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
